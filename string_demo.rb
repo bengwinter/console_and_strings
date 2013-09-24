945a81caa77952ba5ee53411f2b3bda20434a849
Ben Winter <ben.g.winter@gmail.com>
2013-09-24 14:39:04 -0400

    Uploading Madlib

---
+++ string_demo.rb
@@ -0,0 +1,38 @@
+puts "Please type in a noun."
+noun_one = gets.chomp
+
+puts "Please type in a noun."
+noun_two = gets.chomp
+
+puts "I know. One more noun please."
+noun_three = gets.chomp
+
+puts "Now type in an occupation please."
+occupation_one = gets.chomp
+
+puts "I want a verb...NOW"
+verb_one = gets.chomp
+
+puts "Pick your favorite place in the world."
+place_one = gets.chomp
+
+puts "I want a verb but it has to end in ED"
+verb_two = gets.chomp
+
+puts "Now another noun."
+noun_four = gets.chomp
+
+puts "Now an ING verb"
+verb_three = gets.chomp
+
+puts "Now another noun but this one has to be plural."
+noun_five = gets.chomp
+
+puts "Last noun, I promise."
+noun_six = gets.chomp
+
+puts "How do you feel right now? Type in that emotion below."
+emotion_one = gets.chomp
+
+puts "Here is your War Mad Madlib"
+puts "It was during the battle of #{noun_one} when I was running through a #{noun_two} when a #{noun_three} went off right next to my platoon. Our #{occupation_one} yelled for us to #{verb_one} to the nearest #{place_one} we could find. When we got to the #{place_one} we #{verb_two} to start a fire. As we were starting the fire the enemy saw the #{noun_four} from the fire and started #{verb_three} #{noun_five} at us. We all quickly ducked behind the #{noun_six} at the and returned fire. We quickly eliminated the enemy and were #{emotion_one} that we had won the battle."
